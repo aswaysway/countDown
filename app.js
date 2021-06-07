e8ba2b065aa28bea68eace9f53bcdb31a5bc7bc1
aswaysway <kevarez63@gmail.com>
2021-06-07 18:49:50 -0500

    update

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-const countdown = () => {
-    const countDate = new Date('Dec 31, 2021 00:00:00').getTime()
-    const now = new Date().getTime()
-    const gap = countDate - now
+const startCount = () =>{
+    const inputValue = document.getElementById('input-date').value
+    const x = new Date(inputValue).getTime()
 
-    const second = 1000
-    const minute = second * 60
-    const hour = minute * 60
-    const day = hour * 24
-
-    const textDay = Math.floor(gap / day)
-    const textHour = Math.floor((gap % day) / hour)
-    const textMinute = Math.floor((gap % hour) / minute)
-    const textSecond = Math.floor((gap % minute) / second)
-
-    document.querySelector('.day').innerText = textDay
-    document.querySelector('.hour').innerText = textHour
-    document.querySelector('.minute').innerText = textMinute
-    document.querySelector('.second').innerText = textSecond
-
-    if (gap < 1000) {
-        timesup()
+    const countdown = () => {
+        const countDate = new Date(x).getTime()
+        const now = new Date().getTime()
+        const gap = countDate - now
+    
+        const second = 1000
+        const minute = second * 60
+        const hour = minute * 60
+        const day = hour * 24
+    
+        const textDay = Math.floor(gap / day)
+        const textHour = Math.floor((gap % day) / hour)
+        const textMinute = Math.floor((gap % hour) / minute)
+        const textSecond = Math.floor((gap % minute) / second)
+    
+        document.querySelector('.day').innerText = textDay
+        document.querySelector('.hour').innerText = textHour
+        document.querySelector('.minute').innerText = textMinute
+        document.querySelector('.second').innerText = textSecond
+    
+        if (gap < 1000) {
+            timesup()
+        }
     }
+
+    const timesup = () => {
+        document.querySelector('.day').innerText = '0'
+        document.querySelector('.hour').innerText = '0'
+        document.querySelector('.minute').innerText = '0'
+        document.querySelector('.second').innerText = '0'
+    }
+
+    setInterval(countdown, 1000)
 }
 
-const timesup = () => {
-    document.querySelector('.day').innerText = '0'
-    document.querySelector('.hour').innerText = '0'
-    document.querySelector('.minute').innerText = '0'
-    document.querySelector('.second').innerText = '0'
-}
 
-setInterval(countdown, 1000)
